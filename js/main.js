4b96fcb59922763144ae55e549749f40e4ce6ccc
thomas.kaiaua <thomas.kaiaua@gmail.com>
2016-03-03 19:38:27 -0500

    Modified js css and html

--- js/main.js
+++ js/main.js
@@ -7,9 +7,52 @@
    to reference the Bootstrap documentation.)
 */
 
+
 // TODO: Inside of your on ready handler, invoke the Leaflet.js library
 // to draw a map in your `#map-container` div.
 
+ 
+  
+var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 18, attribution: osmAttrib});
+
+var satLayer = L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
+    subdomains: ['otile1','otile2','otile3','otile4']
+});
+
+var drawLayer = L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
+    subdomains: ['otile1','otile2','otile3','otile4']
+});
+
+var mapLayers = {
+    "Satellite": satLayer,
+    "Map View": drawLayer,
+    "Open Street Maps": osm
+}
+
+var map = L.map('map-container').setView([46.883,-121.884], 13);
+
+L.control.layers(mapLayers).addTo(map);
+satLayer.addTo(map);
+
+var marker = L.marker([46.883,-121.884]).addTo(map);
+marker.bindPopup("<b>Mt Rainier National Park</b><br>The highest peak is 4,392 feet high.");
+
+var marker = L.marker([46.878, -121.889]).addTo(map);
+marker.bindPopup("<b>Sunset Park!</b>");
+
+var marker = L.marker([46.888, -121.899]).addTo(map);
+marker.bindPopup("<b>Golden Lakes</b>");
+
+//Places Coordinates 
+//Mount Rainier National Park N46.88316° W121.88455°
+//Sunset Park	N46.87872° W121.88927°
+//Golden Lakes	N46.88874° W121.89996°
+
+  
 // TODO: Add 2 layers to your map you have visuals. Use the Open Street Maps
 // tiles served through the MapQuest CDN. Consult this example to set up
 // the map tiles layers:
@@ -18,4 +61,4 @@
 // TODO: Customize that Map to show markers with popups at no fewer than 3
 // interesting locations. (You'll need to figure out the latitude/longitude for
 // these locations using a mapping tool such as Google Maps.)
-
+  
